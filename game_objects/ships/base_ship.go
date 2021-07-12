a33511bb6aafee94ce9b09c31517667c536e27d6
KristianCh <chovancak.kristian@gmail.com>
2021-07-12 16:25:47 +0200

    render fix and ship progress

---
+++ game_objects/ships/base_ship.go
@@ -0,0 +1,35 @@
+components {
+  id: "base_ship"
+  component: "/game_objects/ships/base_ship.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/images/ships/ships.atlas\"\n"
+  "default_animation: \"battleship\"\n"
+  "material: \"/render/shaders/object_shaders/sprite_modeltype.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
