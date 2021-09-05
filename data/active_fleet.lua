dfc119dd5c2882667727705ce64fc7553dd5cd8b
KristianCh <chovancak.kristian@gmail.com>
2021-09-05 11:56:38 +0200

    added difficulty selection

--- data/active_fleet.lua
+++ data/active_fleet.lua
@@ -2,18 +2,21 @@ local fleet = {
 	{
 		type = 1,
 		level = 1,
+		sub_level = 0,
 		health = 100,
 		is_flagship = true
 	},
 	{
 		type = 2,
 		level = 1,
+		sub_level = 0,
 		health = 100,
 		is_flagship = false
 	},
 	{
 		type = 3,
 		level = 1,
+		sub_level = 0,
 		health = 100,
 		is_flagship = false
 	}
