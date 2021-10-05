7509ccc8316bd46671b493b6f15fe87fd3099065
KristianCh <chovancak.kristian@gmail.com>
2021-10-05 18:59:37 +0200

    added alert system

--- data/active_run_data.lua
+++ data/active_run_data.lua
@@ -1,6 +1,6 @@
 local run_data = {
 	stage = 1,
-	credits = 1000,
+	credits = 100,
 	difficulty = "normal",
 	extra_spawn_rate = 0,
 	extra_credits = 0,
---
+++ game_objects/misc/alert/alert.go
@@ -0,0 +1,30 @@
+components {
+  id: "alert_gui"
+  component: "/game_objects/misc/alert/alert.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "alert_controller"
+  component: "/game_objects/misc/alert/alert_controller.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
